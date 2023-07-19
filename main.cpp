96f5ebbae2f708ba11cf2bb16a681c1e7c649113
JJC <callmejjc@gmail.com>
2023-07-19 09:41:11 +0800

    add code to use network

--- main.cpp
+++ main.cpp
@@ -1,58 +1,102 @@
-//#include "Network.h"
-#define USE_MNIST_LOADER
-#define MNIST_DOUBLE
-#include "mnist.h"
+#include "Network.h"
+#include "Mnist_helper.h"
+#include <iostream>
 
+// #define TRAIN_NETWORK
+#define TEST_NETWORK
 
 void test(){
-    mnist_data *data;
-    unsigned int cnt;
 
-    int ret = mnist_load(R"(E:\Projects\CPlusPlusProjects\testcmake\data\train-images-idx3-ubyte)", R"(E:\Projects\CPlusPlusProjects\testcmake\data\train-labels-idx1-ubyte)", &data, &cnt);
+    Network *network;
+
+    mnist_data *training_data;
+    mnist_data *test_data;
+
+    unsigned int num_training_data = 0;
+    unsigned int num_test_data = 0;
+
+    char* training_data_file = "E:/projects/Cuda/Network/data/train-images-idx3-ubyte";
+    char* training_label_file = "E:/projects/Cuda/Network/data/train-labels-idx1-ubyte";
+    char* test_data_file = "E:/projects/Cuda/Network/data/t10k-images-idx3-ubyte";
+    char* test_label_file = "E:/projects/Cuda/Network/data/t10k-labels-idx1-ubyte";
+
+    int ret = mnist_load(training_data_file, training_label_file, &training_data, &num_training_data);
     if(ret != 0){
-        printf("error\n");
+        std::cout << "Error loading training data" << std::endl;
         return;
     }
+
+    ret = mnist_load(test_data_file, test_label_file, &test_data, &num_test_data);
+    if(ret != 0){
+        std::cout << "Error loading test data" << std::endl;
+        return;
+    }
+
+    // convert
+    double **data = (double**)malloc(sizeof(double*) * num_training_data);
+    unsigned int *label = (unsigned int *)malloc(sizeof(unsigned int) * num_training_data);
+
+    for(int i = 0; i < num_training_data; i++){
+        data[i] = (double*)malloc(sizeof(double) * 784);
+    }
     
-    // print first iamge and label
-    for(int i = 0; i < 28; ++i){
-        for(int j = 0; j < 28; ++j){
-            if(data[0].data[i][j] > 0.1)
-                printf("1");
-            else
-                printf("0");
-        }
-        printf("\n");
+    double **t_data = (double**)malloc(sizeof(double*) * num_test_data);
+    unsigned int *t_label = (unsigned int *)malloc(sizeof(unsigned int) * num_test_data);
+
+    for(int i = 0; i < num_test_data; i++){
+        t_data[i] = (double*)malloc(sizeof(double) * 784);
     }
 
-    printf("%d\n", data[0].label);
+    convert(data, label, training_data, num_training_data);
+    convert(t_data, t_label, test_data, num_test_data);
 
-    double **n_data;
-    n_data = new double*[cnt];
-    for(int i = 0; i < cnt; ++i){
-        n_data[i] = new double[28 * 28];
-        for(int j = 0; j < 28; ++j){
-            for(int k = 0; k < 28; ++k){
-                n_data[i][j * 28 + k] = data[i].data[j][k];
-            }
-        }
+#ifdef TRAIN_NETWORK    
+    // create network
+    int sizes[] = {784, 30, 10};
+    int num_layers = 3;
+    
+    network = new Network(sizes, num_layers);
+
+    // train network
+    network->SGD(data, label, 30, 10, 3.0, t_data, t_label, num_training_data, num_test_data);
+
+    // save network
+    network->saveNetwork();
+
+    // free memory
+    for(int i = 0; i < num_training_data; i++){
+        free(data[i]);
     }
 
-    for(int i = 0; i < 10; i ++){
-        //print
-        for(int x = 0; x < 28; ++x){
-            for(int y = 0; y < 28; ++y){
-                if(n_data[i][x * 28 + y] > 0.1)
-                    printf("1");
-                else
-                    printf("0");
-            }
-            printf("\n");
-        }
+    free(data);
+    free(label);
+    
+    for(int i = 0; i < num_test_data; i++){
+        free(t_data[i]);
     }
+
+    free(t_data);
+    free(t_label);
+
+#else
+
+    #ifdef TEST_NETWORK
+    
+    // load network
+    network = new Network();
+    network->loadNetwork();
+
+    // test network
+
+
+    #else
+    // do nothing
+    printf(
+        "Nothing to do.\n"
+    );
+
+    #endif
+
+#endif
 }
 
-int main(){
-    test();
-    return 0;
-}
